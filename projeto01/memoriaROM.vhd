e11a7defeed0a675940cfd95f54edda562ef06a7
AndreCorreaSantos <acorreasantos2002@gmail.com>
2023-10-13 20:22:40 -0300

    adding

---
+++ projeto01/memoriaROM.vhd
@@ -0,0 +1,59 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity memoriaROM is
+   generic (
+          dataWidth: natural := 13;
+          addrWidth: natural := 9
+    );
+   port (
+          Endereco : in std_logic_vector (addrWidth-1 DOWNTO 0);
+          Dado : out std_logic_vector (dataWidth-1 DOWNTO 0)
+    );
+end entity;
+
+architecture assincrona of memoriaROM is
+  constant NOP  : std_logic_vector(3 downto 0) := "0000";
+  constant LDA  : std_logic_vector(3 downto 0) := "0001";
+  constant SOMA : std_logic_vector(3 downto 0) := "0010";
+  constant SUB  : std_logic_vector(3 downto 0) := "0011";
+  constant LDI  : std_logic_vector(3 downto 0) := "0100";
+  constant STA : std_logic_vector(3 downto 0) := "0101";
+  constant JMP : std_logic_vector(3 downto 0) := "0110";
+  constant JEQ : std_logic_vector(3 downto 0) := "0111";
+  constant CEQ : std_logic_vector(3 downto 0) := "1000";
+  constant JSR : std_logic_vector(3 downto 0) := "1001";
+  constant RET : std_logic_vector(3 downto 0) := "1010";
+  constant OP_AND : std_logic_vector(3 downto 0) := "1011";
+  
+  
+  
+  type blocoMemoria is array(0 TO 2**addrWidth - 1) of std_logic_vector(dataWidth-1 DOWNTO 0);
+
+  function initMemory
+        return blocoMemoria is variable tmp : blocoMemoria := (others => (others => '0'));
+  begin
+        -- Inicializa os endereços:
+        tmp(0) := STA & "111111111";
+		  tmp(1) := LDI & "000000001";
+		  tmp(2) := STA & "000000001";
+		  tmp(3) := NOP & "000000000";
+		  tmp(4) := LDA & "101100000";
+		  tmp(5) := STA & "100100000";
+		  tmp(6) := OP_AND & "000000001";
+		  tmp(7) := STA & "100100001";
+		  tmp(8) := NOP & "000000000";
+		  tmp(9) := JMP & "000000011";
+		  
+		  
+		  
+		  
+        return tmp;
+    end initMemory;
+
+    signal memROM : blocoMemoria := initMemory;
+
+begin
+    Dado <= memROM (to_integer(unsigned(Endereco)));
+end architecture;
